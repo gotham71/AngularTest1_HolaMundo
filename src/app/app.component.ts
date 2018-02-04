83c594ccb840696aba3febdd9b598941b95ab5da
Jose Ramirez <josemaramirez@hotmail.com>
2018-02-04 11:31:08 +0100

    Structure: header(navbar), body, footer. Components, etc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  nombre:string = "Jose María";
-  apellido:string = "Ramírez Baena";
+  nombre: string = "Jose María";
+  apellido: string = "Ramírez Baena";
 }
